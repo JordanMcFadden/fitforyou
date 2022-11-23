07cb213144d9282fdc0d1657ebc6e5a40bc6091b
JordanMcFadden <jordan.d.mcfadden-1@ou.edu>
2022-11-23 14:25:29 -0600

    Refinig Login page

--- src/Components/Pages/Login/Login.js
+++ src/Components/Pages/Login/Login.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useContext } from 'react';
+import AuthContext from '../../../Context/AuthProvider';
+import Axios from '../../../Api/Axios';
+import axios from 'axios';
+
+const LOGIN_URL = '/auth';
 
 const Login = () => {
+    const { setAuth } = useContext(AuthContext);
     const userRef = useRef();
     const errRef = useRef();
 
@@ -21,10 +27,43 @@ const Login = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(user, pwd);
-        setUser('');
-        setPwd('');
-        setSuccess(true);
+
+        try{
+            const response = await axios.post(LOGIN_URL, JSON.stringify({user, pwd}),
+                {
+                    headers: {'Content-Type': 'application/json'},
+                    withCredentials: true
+                }
+            );
+            console.log(JSON.stringify(response ?.data));
+           // console.log(JSON.stringify(response ?.data));
+           const accessToken = response?.data?.accessToken;
+           const roles = respnse?.data?.roles;
+           setAuth({ user, pwd, roles, accessToken });
+            setUser('');
+            setPwd('');
+            setSuccess(true);
+        } catch (err) {
+            if(!err?.response) 
+            {
+                setErrMsg('No Server Reponse')
+            }
+            else if(err.response?.status===400)
+            {
+                setErrMsg('Missing Username or Password');
+            }
+            else if(err.response?.status === 401) 
+            {
+                setErrMsg('Unauthorized');
+            }
+            else
+            {
+                setErrMsg('Login Failed');
+            }
+            errRef.current.focus();
+        }
+
+       
     }
 
     return (
